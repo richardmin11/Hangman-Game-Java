79e52531fd0d6b2f37b9c31637dec850e370a02b
richardmin11 <39314479+richardmin11@users.noreply.github.com>
2018-05-15 17:15:13 -0400

    Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -1,15 +1,13 @@
 public class Hangman {
 	public static void main(String [] args) {	
 		
-    Game game = new Game("treehouse");
-		Prompter prompter = new Prompter(game);
+	    Game game = new Game("treehouse");
+	    Prompter prompter = new Prompter(game);
 		
-    while(game.getRemainingTries() > 0 && !game.isWon()) {
-			prompter.displayProgress();
-			prompter.promptForGuess();
-		}
-    
-		prompter.displayOutcome();
-    
+	    while(game.getRemainingTries() > 0 && !game.isWon()) {
+				prompter.displayProgress();
+				prompter.promptForGuess();
+	    }
+	    prompter.displayOutcome();   
 	}
 }		
